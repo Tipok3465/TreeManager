2dfbeba1ce977168fead157147a7eb15d0e42a16
noname <108078194+Tipok3465@users.noreply.github.com>
2024-05-15 08:44:14 +0300

    COMPLETE

--- RBTree.h
+++ RBTree.h
@@ -7,7 +7,7 @@ class RBTree {
 public:
     enum Color { RED, BLACK };
     struct Node {
-        int data;
+        int64_t value;
         Color color;
         Node *left;
         Node *right;
@@ -19,24 +19,26 @@ public:
 
 private:
     Node *root;
-    Node* find(Node *node, int key);
+    Node* find(Node *node, int64_t key);
     void fixDelete(Node *x);
     void rbTransplant(Node *u, Node *v);
-    void deleteNode(Node *node, int key);
+    void remove(Node *node, int64_t key);
     void fixInsert(Node *k);
     static void drawTree(Node *x, QGraphicsScene *&scene);
 
 public:
     RBTree();
-    Node* contains(int k);
+    ~RBTree();
+    void deleteTree(Node* cur);
+    Node* contains(int64_t k);
     void clickDelete(Node *cur, int x, int y);
     static void render(QGraphicsScene *scene, Node *_root);
-    Node *minimum(Node *node);
+    void delAllTree();
     Node *maximum(Node *node);
     void leftRotate(Node *x);
     void rightRotate(Node *x);
-    void insert(int key);
+    void insert(int64_t key);
     Node *getRoot();
-    void deleteNode(int data);
+    void deleteNode(int64_t data);
 
 };
